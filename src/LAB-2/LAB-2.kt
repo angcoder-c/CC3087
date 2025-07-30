d68cce0e45b8f3017b9a985166deeedbcfa3c277
angcoder-c <angcoder.c@gmail.com>
2025-07-30 16:09:14 -0600

    Programa finalizado, probado en el notebook  de pruebas unitarias

--- src/LAB-2/LAB-2.kt
+++ src/LAB-2/LAB-2.kt
@@ -1,9 +1,68 @@
-package `LAB-2`
+/*
+* Angel Gabriel Chavez Otzoy
+* 24248
+* Programacion de plataformas moviles
+* https://github.com/angcoder-c/CC3087/blob/main/src/LAB-2/LAB-2.kt
+* */
+
+fun main() {
+    val inputList = listOf(10, "Hola", null, false, 7, "PC", true, 15.5)
+    val result = processList(inputList)
+    println("Resultado:")
+    if (result != null) {
+        for (item in result) {
+            println(item)
+        }
+    }
+}
+
+data class ItemData(
+    val originalPos: Int,
+    val originalValue: Any,
+    val type: ElementType,
+    val info: String
+) {
+    // item data to string
+    override fun toString(): String {
+        // value to string
+        val value = when (originalValue) {
+            is String -> "'$originalValue'"
+            else -> "'$originalValue'"
+        }
+        val typeStr = elementType2String(type)
+        return "$value estaba en la posición $originalPos, es de tipo $typeStr e info es $info".lowercase()
+    }
+}
 
 enum class ElementType {
-    ENTERO,
     CADENA,
-    BOOLEANO
+    ENTERO,
+    BOOLEANO,
+    DESCONOCIDO
+}
+
+fun processList(inputList: List<Any?>?): MutableList<ItemData>? {
+    if (inputList == null) {
+        return null
+    }
+    // resultado
+    val listaMutable = mutableListOf<ItemData>()
+    for ((index, item) in inputList.withIndex()) {
+        if (item != null) {
+            val type = type2ElementType(item)
+            val info = getInfoByItem(item)
+            listaMutable.add(
+                ItemData(
+                    index,
+                    item,
+                    type,
+                    info
+                )
+            )
+        }
+    }
+
+    return listaMutable
 }
 
 fun elementType2String (type: ElementType): String {
@@ -16,33 +75,28 @@ fun elementType2String (type: ElementType): String {
     }
 }
 
-class ItemData(
-    var originalPos: Int,
-    var originalValue: Any?,
-    var type: ElementType,
-    var info: String?
-) {
-    // item data to string
-    override fun toString(): String {
-        // value to string
-        val value = when (originalValue) {
-            is String -> "'${originalValue}'"
-            else -> originalValue.toString()
+fun type2ElementType (item: Any?): ElementType {
+        // asignar un element type segun el tipo del item
+        return when (item) {
+            is Int -> ElementType.ENTERO
+            is String -> ElementType.CADENA
+            is Boolean -> ElementType.BOOLEANO
+            else -> ElementType.DESCONOCIDO
         }
-
-        val typeStr = elementType2String(type)
-        return "$value estaba en la posición $originalPos, es de tipo $typeStr e info es ${info}"
     }
-}
 
-fun processList(list: List<Any?>?): MutableList<ItemData>? {
-    if (list == null) {
-        return null
+
+fun getInfoByItem(item: Any): String {
+    // obtener informacion en string segun el tipo del item
+    return when (item) {
+        is Int -> when {
+            item % 10 == 0 -> "M10"
+            item % 5 == 0 -> "M5"
+            item % 2 == 0 -> "M2"
+            else -> "-"
+        }
+        is String -> "L${item.length}"
+        is Boolean -> if (item) "verdadero" else "falso"
+        else -> "desconocido"
     }
-    val result = mutableListOf<ItemData>()
-    return result
-}
-
-fun main() {
-
 }
